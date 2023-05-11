1fc70946fd3e53ca90ff35ce76d7ddeaff4906d4
sbaldu <simone.balducci00@gmail.com>
2023-05-11 11:52:23 +0200

    Add solutions to day 4 and 5 of AoC 2022

---
+++ 2022/Day04/solution.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+def get_range(assignment: str):
+    domain = assignment.split('-')
+    first = int(domain[0])
+    second = int(domain[1])
+    return (first, second)
+
+def contains(range_1: tuple, range_2: tuple):
+    # The first range is contained in the second
+    if range_1[0] >= range_2[0] and range_1[1] <= range_2[1]:
+        return True
+    # The second range is contained in the first
+    if range_1[0] <= range_2[0] and range_1[1] >= range_2[1]:
+        return True
+    return False
+
+def overlap(range_1: tuple, range_2: tuple):
+    list_range_1 = np.arange(range_1[0], range_1[1]+1)
+    list_range_2 = np.arange(range_2[0], range_2[1]+1)
+
+    len_1 = len(list_range_1)
+    len_2 = len(list_range_2)
+    if len_1 < len_2:
+        for element in list_range_1:
+            if element in list_range_2:
+                return True
+    else:
+        for element in list_range_2:
+            if element in list_range_1:
+                return True
+
+
+file = open('./input.txt', 'r')
+contained_counter = 0
+overlap_counter = 0
+for line in file:
+    assignments = line.split(',')
+    range1 = get_range(assignments[0])
+    range2 = get_range(assignments[1])
+    if contains(range1, range2):
+        contained_counter += 1
+    if overlap(range1, range2):
+        overlap_counter += 1
+
+print("The number of ranges contained in another is:", contained_counter)
+print("The number of ranges overlapping with another is:", overlap_counter)
---
+++ 2022/Day05/solution.cc
@@ -0,0 +1,153 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+void create_stacks(std::vector<std::vector<char>>& stacks, std::string const& path) {
+  std::ifstream file(path);
+  std::string file_row;
+  char letter;
+  std::string temp;
+
+  int number_of_stacks{};
+  while(true) {
+	// Write the row from the file inside a string
+	getline(file, file_row);
+	if (number_of_stacks == 0) {
+	  number_of_stacks = (file_row.size()+1)/4;
+	  stacks.resize(number_of_stacks);
+	}
+	if (file_row == "") {
+	  break;
+	}
+
+	for (int i{}, j{}; i < number_of_stacks; ++i, j+=4) {
+	  std::string crate = file_row.substr(j, 4);
+	  // Clean the crate string
+      crate.erase(std::remove(crate.begin(), crate.end(), '['), crate.end());
+      crate.erase(std::remove(crate.begin(), crate.end(), ']'), crate.end());
+      crate.erase(std::remove(crate.begin(), crate.end(), ' '), crate.end());
+	  
+	  if (crate != "") {
+		stacks[i].push_back(crate[0]);
+	  }
+	}
+  }
+  for (auto& stack : stacks) {
+	stack.pop_back();
+  }
+}
+
+template <typename T>
+void flip_vector(std::vector<T>& vec) {
+  std::size_t size{vec.size()};
+  std::vector<T> temp(size);
+
+  for (int i{}; i < size; ++i) {
+	temp[i] = vec[size-1-i];
+  }
+
+  vec = temp;
+}
+
+void move(std::vector<std::vector<char>>& stacks,
+		  int n_crates,
+		  int starting_crate,
+		  int ending_crate) {
+  for (int i{}; i < n_crates; ++i) {
+	stacks[ending_crate-1].push_back(*(stacks[starting_crate-1].end()-1));
+	stacks[starting_crate-1].pop_back();
+  }
+}
+
+void new_move(std::vector<std::vector<char>>& stacks,
+		  int n_crates,
+		  int starting_crate,
+		  int ending_crate) {
+  for (int i{1}; i <= n_crates; ++i) {
+	stacks[ending_crate-1].push_back(*(stacks[starting_crate-1].end()-1-n_crates+i));
+  }
+  for (int i{}; i < n_crates; ++i) {
+	stacks[starting_crate-1].pop_back();
+  }
+}
+
+void find_final_stacks(std::vector<std::vector<char>>& stacks,
+					   std::string const& file_path) {
+  create_stacks(stacks, file_path);
+	for (auto& stack : stacks) {
+	  flip_vector(stack);
+	}
+
+	std::ifstream file(file_path);
+	std::string file_row;
+	std::string word;
+	std::string crate;
+	std::string start;
+	std::string end;
+	while (getline(file, file_row)) {
+	  std::stringstream row_stream(file_row);
+	  getline(row_stream, word, ' ');
+
+	  if (word != "move") { continue; }
+
+	  getline(row_stream, crate, ' ');
+	  getline(row_stream, word, ' ');
+	  getline(row_stream, start, ' ');
+	  getline(row_stream, word, ' ');
+	  getline(row_stream, end);
+
+	  move(stacks, std::stoi(crate), std::stoi(start), std::stoi(end));
+	  std::cout << "------------------------------" << std::endl;
+	  for (auto const& stack : stacks) {
+		std::cout << *(stack.end()-1) << std::endl;
+	  }
+	  std::cout << "------------------------------" << std::endl;
+	}
+}
+
+void find_new_final_stacks(std::vector<std::vector<char>>& stacks,
+						   std::string const& file_path) {
+  create_stacks(stacks, file_path);
+  for (auto& stack : stacks) {
+	flip_vector(stack);
+  }
+
+  std::ifstream file(file_path);
+  std::string file_row;
+  std::string word;
+  std::string crate;
+  std::string start;
+  std::string end;
+  while (getline(file, file_row)) {
+	std::stringstream row_stream(file_row);
+	getline(row_stream, word, ' ');
+
+	if (word != "move") { continue; }
+
+	getline(row_stream, crate, ' ');
+	getline(row_stream, word, ' ');
+	getline(row_stream, start, ' ');
+	getline(row_stream, word, ' ');
+	getline(row_stream, end);
+
+	new_move(stacks, std::stoi(crate), std::stoi(start), std::stoi(end));
+	std::cout << "------------------------------" << std::endl;
+	for (auto const& stack : stacks) {
+	  std::cout << *(stack.end()-1) << std::endl;
+	}
+	std::cout << "------------------------------" << std::endl;
+  }
+}
+
+int main() {
+  std::vector<std::vector<char>> stacks;
+  std::vector<std::vector<char>> new_stacks;
+  const std::string file_path{"./input.txt"};
+
+  find_final_stacks(stacks, file_path);
+  find_new_final_stacks(new_stacks, file_path);
+}
